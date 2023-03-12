38bae3e8dfb0efb94170f0a44c0a489294cafb81
forni.luis <forni.luis@gmail.com>
2023-03-12 19:13:22 +0100

    first commit

--- prueba_tecnica/core/views.py
+++ prueba_tecnica/core/views.py
@@ -22,46 +22,50 @@ def estadisticasCompras(request):
     # DataFrame órdenes de compra
     df_bids = pd.DataFrame(datos_json['bids']).fillna(0)
 
-    # El valor medio de las órdenes, donde el valor es la cantidad de la orden multiplicado por su precio.
-    value_bids = df_bids['qty'] * df_bids['px']
-    average_value = value_bids.mean()
+    try:
+        # El valor medio de las órdenes, donde el valor es la cantidad de la orden multiplicado por su precio.
+        value_bids = df_bids['qty'] * df_bids['px']
+        average_value = value_bids.mean()
 
-    # greater_value. La orden de compra con mayor valor
-    gv_px = df_bids['px'].max()
-    gv_qty = df_bids['qty'].max()
-    gv_num = df_bids['num'].max()
-    gv_value = gv_px * gv_qty
+        # greater_value. La orden de compra con mayor valor
+        gv_px = df_bids['px'].max()
+        gv_qty = df_bids['qty'].max()
+        gv_num = df_bids['num'].max()
+        gv_value = gv_px * gv_qty
 
-    # lesser_value. La orden de compra con menor valor.
-    lv_px = df_bids['px'].min()
-    lv_qty = df_bids['qty'].min()
-    lv_num = df_bids['num'].min()
-    lv_value = lv_px * lv_qty
+        # lesser_value. La orden de compra con menor valor.
+        lv_px = df_bids['px'].min()
+        lv_qty = df_bids['qty'].min()
+        lv_num = df_bids['num'].min()
+        lv_value = lv_px * lv_qty
 
-    # El total de monedas en órdenes.
-    total_qty = df_bids['qty'].sum()    
+        # El total de monedas en órdenes.
+        total_qty = df_bids['qty'].sum()    
 
-    # El precio total de las órdenes.
-    total_px = df_bids['px'].sum()  
-    
-    greater_value = {}
-    greater_value['px'] = str(gv_px)
-    greater_value['qty'] = str(gv_qty)
-    greater_value['num'] = str(gv_num)
-    greater_value['value'] = str(gv_value)
-    
-    lesser_value = {}
-    lesser_value['px'] = str(lv_px)
-    lesser_value['qty'] = str(lv_qty)
-    lesser_value['num'] = str(lv_num)
-    lesser_value['value'] = str(lv_value)
+        # El precio total de las órdenes.
+        total_px = df_bids['px'].sum()  
+        
+        greater_value = {}
+        greater_value['px'] = str(gv_px)
+        greater_value['qty'] = str(gv_qty)
+        greater_value['num'] = str(gv_num)
+        greater_value['value'] = str(gv_value)
+        
+        lesser_value = {}
+        lesser_value['px'] = str(lv_px)
+        lesser_value['qty'] = str(lv_qty)
+        lesser_value['num'] = str(lv_num)
+        lesser_value['value'] = str(lv_value)
 
-    bids = {}
-    bids['average_value'] = average_value
-    bids['greater_value'] = greater_value
-    bids['lesser_value'] = lesser_value
-    bids['total_qty'] = total_qty
-    bids['total_px'] = total_px
+        bids = {}
+        bids['average_value'] = average_value
+        bids['greater_value'] = greater_value
+        bids['lesser_value'] = lesser_value
+        bids['total_qty'] = total_qty
+        bids['total_px'] = total_px
+
+    except:
+        bids = []
 
     datos = {'bids': bids}
 
@@ -75,46 +79,50 @@ def estadisticasVentas(request):
     # DataFrame órdenes de venta
     df_asks = pd.DataFrame(datos_json['asks']).fillna(0)
     
-    # El valor medio de las órdenes, donde el valor es la cantidad de la orden multiplicado por su precio.
-    value_asks = df_asks['qty'] * df_asks['px']
-    average_value = value_asks.mean() 
+    try:
+        # El valor medio de las órdenes, donde el valor es la cantidad de la orden multiplicado por su precio.
+        value_asks = df_asks['qty'] * df_asks['px']
+        average_value = value_asks.mean() 
 
-    # greater_value. La orden de venta con mayor valor
-    gv_px = df_asks['px'].max()
-    gv_qty = df_asks['qty'].max()
-    gv_num = df_asks['num'].max()
-    gv_value = gv_px * gv_qty
+        # greater_value. La orden de venta con mayor valor
+        gv_px = df_asks['px'].max()
+        gv_qty = df_asks['qty'].max()
+        gv_num = df_asks['num'].max()
+        gv_value = gv_px * gv_qty
 
-    # lesser_value. La orden de venta con menor valor.
-    lv_px = df_asks['px'].min()
-    lv_qty = df_asks['qty'].min()
-    lv_num = df_asks['num'].min()
-    lv_value = lv_px * lv_qty
+        # lesser_value. La orden de venta con menor valor.
+        lv_px = df_asks['px'].min()
+        lv_qty = df_asks['qty'].min()
+        lv_num = df_asks['num'].min()
+        lv_value = lv_px * lv_qty
 
-    # El total de monedas en órdenes.
-    total_qty = df_asks['qty'].sum()    
+        # El total de monedas en órdenes.
+        total_qty = df_asks['qty'].sum()    
 
-    # El precio total de las órdenes.
-    total_px = df_asks['px'].sum()  
+        # El precio total de las órdenes.
+        total_px = df_asks['px'].sum()  
+        
+        greater_value = {}
+        greater_value['px'] = str(gv_px)
+        greater_value['qty'] = str(gv_qty)
+        greater_value['num'] = str(gv_num)
+        greater_value['value'] = str(gv_value)
+        
+        lesser_value = {}
+        lesser_value['px'] = str(lv_px)
+        lesser_value['qty'] = str(lv_qty)
+        lesser_value['num'] = str(lv_num)
+        lesser_value['value'] = str(lv_value)
+
+        asks = {}
+        asks['average_value'] = average_value
+        asks['greater_value'] = greater_value
+        asks['lesser_value'] = lesser_value
+        asks['total_qty'] = total_qty
+        asks['total_px'] = total_px
     
-    greater_value = {}
-    greater_value['px'] = str(gv_px)
-    greater_value['qty'] = str(gv_qty)
-    greater_value['num'] = str(gv_num)
-    greater_value['value'] = str(gv_value)
-    
-    lesser_value = {}
-    lesser_value['px'] = str(lv_px)
-    lesser_value['qty'] = str(lv_qty)
-    lesser_value['num'] = str(lv_num)
-    lesser_value['value'] = str(lv_value)
-
-    asks = {}
-    asks['average_value'] = average_value
-    asks['greater_value'] = greater_value
-    asks['lesser_value'] = lesser_value
-    asks['total_qty'] = total_qty
-    asks['total_px'] = total_px
+    except:
+        asks = []
 
     datos = {'asks': asks}
 
